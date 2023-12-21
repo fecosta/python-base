1724f2dc7212ae086a9e2a58a7799a5da52f8258
Felipe Costa <felipecosta@fecosta.local>
2023-12-21 19:50:55 -0300

    ajustando formatação e add gitignore

--- hello.py
+++ hello.py
@@ -6,12 +6,12 @@ Dependendo da língua configurada no ambiente o programa exibe a mensagem corres
 Como usar:
 
 Tenha a variável LANG devidamente configurada ex:
-	export LANG=pt_BR
+    export LANG=pt_BR
 
 Execução
-	pyhthon3 hello.py
-	ou
-	./hello.py
+    pyhthon3 hello.py
+    ou
+    ./hello.py
 """
 __version__ = "0.0.1"
 __author__ = "Felipe Costa"
@@ -25,12 +25,12 @@ current_language = os.getenv("LANG","en_US")[:5]
 msg = "Hello, World!"
 
 if current_language == "pt_BR":
-	msg = "Olá, Mundo!"
+    msg = "Olá, Mundo!"
 elif current_language == "it_IT":
-	msg = "Ciao, Mondo!"
+    msg = "Ciao, Mondo!"
 elif current_language == "es_SP":
-	msg = "Hola, Mundo!"
+    msg = "Hola, Mundo!"
 elif current_language == "fe_FR":
-	msg = "Bonjour, Monde!"
-	
+    msg = "Bonjour, Monde!"
+
 print(msg)
