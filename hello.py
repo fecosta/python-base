45694546fa1e73ae2974a7652e58a0309adf67e7
Felipe Costa <felipecosta@fecosta.local>
2023-12-21 18:11:06 -0300

    adicionando outros idiomas a partir da variavel de ambiente

--- hello.py
+++ hello.py
@@ -1,4 +1,36 @@
 #!/usr/bin/env python3
+"""Hello world multi linguas.
 
-print("Hello, World!")
+Dependendo da língua configurada no ambiente o programa exibe a mensagem correspondente
 
+Como usar:
+
+Tenha a variável LANG devidamente configurada ex:
+	export LANG=pt_BR
+
+Execução
+	pyhthon3 hello.py
+	ou
+	./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Felipe Costa"
+__license__ = "unlicense"
+
+
+import os
+
+current_language = os.getenv("LANG","en_US")[:5]
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+	msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+	msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+	msg = "Hola, Mundo!"
+elif current_language == "fe_FR":
+	msg = "Bonjour, Monde!"
+	
+print(msg)
